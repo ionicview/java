c6c98e2cf95a36be8a04349460d5a388985d9d04
ionicview <ionicview@163.com>
2018-08-20 00:44:00 +0900

    问答题初版完成

--- youranxue-domain/src/main/java/com/youranxue/domain/vo/QuestionShortAnswerVO.java
+++ youranxue-domain/src/main/java/com/youranxue/domain/vo/QuestionShortAnswerVO.java
@@ -1,5 +1,23 @@
 package com.youranxue.domain.vo;
 
-public class QuestionShortAnswerVO {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import lombok.Data;
+
+@Data
+public class QuestionShortAnswerVO {
+	long shortAnswerId;
+	String shortAnswerContent;
+	List<String> images = new ArrayList<>();
+	List<QuestionShortAnswerSubQuestionVO> subQuestionList;
+	QuestionAnswerVO answer;
+
+	public void addImages(String... images) {
+		if (null == images || 0 == images.length) {
+			return;
+		}
+		this.images.addAll(Arrays.asList(images));
+	}
 }
